b575d74c7b62f540e0d0868c12ef11f89c19bbdf
Ferenc Grecu <mivor20@gmail.com>
2014-01-07 01:07:54 +0200

    Spike of View class

---
+++ GeomComp/FormView.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GeomComp
+{
+    partial class View
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Frame = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.Frame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Frame
+            // 
+            this.Frame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(54)))), ((int)(((byte)(66)))));
+            this.Frame.Location = new System.Drawing.Point(100, 0);
+            this.Frame.Name = "Frame";
+            this.Frame.Size = new System.Drawing.Size(720, 480);
+            this.Frame.TabIndex = 0;
+            this.Frame.TabStop = false;
+            this.Frame.Paint += new System.Windows.Forms.PaintEventHandler(this.Frame_Paint);
+            // 
+            // View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(43)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(834, 492);
+            this.Controls.Add(this.Frame);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(148)))), ((int)(((byte)(150)))));
+            this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.Name = "View";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "GeomComp";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.Frame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Frame;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ GeomComp/FormView.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeomComp
+{
+    public partial class View : Form
+    {
+        private Bitmap image;
+
+        private Color appBckg;
+        private Color imgBckg;
+        private Color altContent;
+        private Color base00;
+        private Color content;
+        private Color empContent;
+        private Color base2;
+        private Color base3;
+        private Color yellow;
+        private Color orange;
+        private Color red;
+        private Color magenta;
+        private Color violet;
+        private Color blue;
+        private Color cyan;
+        private Color green;
+
+        public View()
+        {
+            InitializeComponent();
+
+            appBckg = Color.FromArgb(0, 43, 54);
+            imgBckg = Color.FromArgb(7, 54, 66);
+            altContent = Color.FromArgb(88, 110, 117);
+            base00 = Color.FromArgb(101, 123, 131);
+            content = Color.FromArgb(131, 148, 150);
+            empContent = Color.FromArgb(147, 161, 161);
+            base2 = Color.FromArgb(238, 232, 213);
+            base3 = Color.FromArgb(253, 246, 227);
+            yellow = Color.FromArgb(181, 137, 0);
+            orange = Color.FromArgb(203, 75, 22);
+            red = Color.FromArgb(220, 50, 47);
+            magenta = Color.FromArgb(211, 54, 130);
+            violet = Color.FromArgb(108, 113, 196);
+            blue = Color.FromArgb(38, 139, 210);
+            cyan = Color.FromArgb(42, 161, 152);
+            green = Color.FromArgb(133, 153, 0);
+
+            image = new Bitmap(Frame.Width, Frame.Height);
+            using (Graphics gx = Graphics.FromImage(image))
+            {
+                // uncomment for higher quality output
+                gx.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+                gx.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                gx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                gx.Clear(imgBckg);
+                gx.DrawEllipse(new Pen(red), 10, 10, 333, 333);
+                gx.DrawRectangle(new Pen(green), 50, 50, 200, 200);
+            }
+        }
+
+        private void Frame_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(image, 0, 0);
+        }
+    }
+}
