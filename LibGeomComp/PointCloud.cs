42574cc195a708004f03736d066e3d366c2e3f5f
Ferenc Grecu <mivor20@gmail.com>
2014-01-16 04:20:09 +0200

    Add tests and spike of PointCloud

---
+++ LibGeomComp/PointCloud.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace LibGeomComp
+{
+    public class PointCloud
+    {
+        public List<Point> Points {get; private set;}
+
+        public PointCloud(IEnumerable<Point> points)
+        {
+            // TODO: Complete member initialization
+            this.Points = new List<Point>(points);
+        }
+
+        public PointCloud(IEnumerable<int> valuesX, IEnumerable<int> valuesY)
+        {
+            Points = new List<Point>(valuesX.Zip<int, int, Point>(valuesY, (x, y) => new Point(x, y)));
+        }
+
+        public List<Point> GetSimpleMinAreaRect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestGeomComp/TestPointCloud.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LibGeomComp;
+using System.Drawing;
+
+namespace TestGeomComp
+{
+    [TestFixture]
+    public class TestPointCloud
+    {
+        PointCloud cloud;
+        IEnumerable<Point> result;
+        List<Point> expected;
+
+        [SetUp]
+        public void SetUp()
+        {
+            expected = new List<Point>();
+        }
+
+        [Test]
+        public void ConstructorArraysToList()
+        {
+            int[] valX = new int[] { 20, 35, 50 };
+            int[] valY = new int[] { 20, 50, 15 };
+
+            expected.Add(new Point(20, 20));
+            expected.Add(new Point(35, 50));
+            expected.Add(new Point(50, 15));
+
+            cloud = new PointCloud(valX, valY);
+
+            result = cloud.Points;
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        [Test]
+        public void SimpleMinAreaRect()
+        {
+            int[] valX = new int[] { 20, 35, 50 };
+            int[] valY = new int[] { 20, 50, 15 };
+
+            expected.Add(new Point(20, 15));
+            expected.Add(new Point(20, 50));
+            expected.Add(new Point(50, 15));
+            expected.Add(new Point(50, 50));
+
+            cloud = new PointCloud(valX, valY);
+
+            result = cloud.GetSimpleMinAreaRect();
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
